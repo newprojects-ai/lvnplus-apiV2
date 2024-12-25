d1141f9679ee4551c282613ddcf36b08af48a2ff
genid-ss <genid.ss@gmail.com>
2024-12-25 13:32:36 +0000

    CORS - Add new UI port 3001

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173','http://localhost:3001'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
